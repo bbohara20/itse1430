0d82642fce31387bcb5a9dd8dce1f295cb64d40d
bam bohara <bam.bohara@my.tccd.edu>
2020-08-30 22:35:10 -0500

    playing round with debugging

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -1,5 +1,5 @@
 ﻿/* 
- * Michael Taylor
+ * Bam Bohara
  * ITSE 1430
  * Lab 1
  */
@@ -7,47 +7,22 @@ using System;
 
 namespace HelloWorld
 {
-    //Pascal casing - Capitalize on word boundaries including first word
-    //Camel casing - Capitalize on word boundaries except first (e.g. firstName, payRate)
     class Program
-    {
-        //Function declaration - declaration to compiler that a function exists with a given signature
-        //Function signature - function name and the parameter types (sometimes it will include return type)
-        //Function definition - declaration + implementation
+    { 
         static void Main ( string[] args )  // 1 parameter 
         {
             // A single line comment
-            // Another line of comments
-
-            //Display given string to output 
-            //Arguments - data passed to function
+            // Another line of commen
             Console.WriteLine("Hello World!"); //printf, cout
 
-            //Variable declaration: 
-            // T id;
-            //int hours;
-            //hours = 10;  //Assignment operator: id = E
+            int hours = 40;
+            double payRate = 12.80;
 
-            //Identifier Rules
-            // 1. Unique within scope
-            // 2. Must start with underscore or letter
-            // 3. Consist of letters, digits and underscores
-            // e.g. 
-            //   Valid? firstName, first_Name, first_name
-            //   pay1Rate
-            //   1chance
+            double totalPay = hours * payRate;
 
-            //Always camel case local variables, noun            
-            //Preferred: T id = E;
-            int hours = 10;
-
-            //int pay = 0;
-            //pay = hours * 9;
-            int totalPay = hours * 9;
-
-            //Function overloading - multiple functions with same name but different parameters
-            // atof, atoi 
-            Console.WriteLine(totalPay);
+            // Another block
+            // 10 + 45;
+            // No !!! { console.writeLine(); }
         }
     }
 }
