db16d4eb71866e769e281499f53b7745927e8cf1
bam bohara <bam.bohara@my.tccd.edu>
2020-08-31 16:59:03 -0500

    playing round with c# coding

---
+++ classwork/HelloFrind/HelloFrind/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloFrind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFriend = "mike";
+            Console.WriteLine( myFriend);
+            Console.WriteLine("Hello " + myFriend);
+            Console.WriteLine("Hello " + myFriend + " How are you");
+
+
+
+        }
+    }
+}
