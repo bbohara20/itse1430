6e6a609e273e892a03091ea329f42a383f536583
bam bohara <bam.bohara@my.tccd.edu>
2020-09-05 22:25:11 -0500

    writing and Reading data  menu "movie Library"

--- classwork/MovieLibrary/MovieLibrary/Program.cs
+++ classwork/MovieLibrary/MovieLibrary/Program.cs
@@ -1,53 +1,48 @@
-﻿
+﻿/* 
+ * Bam bohara
+ * ITSE 1430 
+ * Movie Library consle Application 
+ */
 using System;
 
 
 namespace MovieLibrary
 {
-    class Program
+    internal class Program
     {
-        static void Main ( string[] args )
+        private static void Main ( string[] args )
         {
-           
-            FunWithvariables();
+            //FunWithTypes()    
+            // FunWithvariables();
+            DisplayMenu();
+            string title = "";
+            string discription = "";
+            string rating = "";
+            int duration;
+          }
 
-        }
+         static char DisplayMenu ()
+         {
+            // do s while (E);
+            do
+            {
 
-        private static void FunWithvariables ()
-        {
-            // Declares hours of type int with int initial value 
-            int hours = 10;
-            // Definitely assigned rule 
-            int value;
-            value = 10;
-            int calcualateValue = value *  10;
+                Console.WriteLine("Movie Library");
+                Console.WriteLine("------------------");
 
-            // Identifiers rule 
-            // Must start with underScore or a letter 
-            // Must consist of letters, digits or underScore 
-            // Must be unique within scope
-            // Cannot be a keyword
-            // variable names 
-            // CamelCased - local variables and parameters 
-            // nouns
-            // descriptive and no abbreviations or acronyms (e.g. okButton, nbrPeople
-            //Multiple variable declaration
-            int width, length;
-            int grade1 = 50, grade2= 60;
-            //Block declarations
-            int x, y, z;
-            double rate1, rate2;
-            string name1, name2;
-            //...
-            name2 = "";
+                Console.WriteLine("Q)uit");
+                // Get the input from user
+                string input = Console.ReadLine();
+                if (input == "Q")
+                    return 'Q';
 
-
+                Console.WriteLine("Invalid option");
+            } while (true);
             
 
-        }
+           }
 
-       
-        
+           }
 
         }
-    }
+     
