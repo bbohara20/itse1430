d6e73461ec291486bcd5ff290feff3ba793fad1c
bam bohara <bam.bohara@my.tccd.edu>
2020-09-10 18:59:24 -0500

    Display menu function

--- classwork/MovieLibrary/MovieLibrary/Program.cs
+++ classwork/MovieLibrary/MovieLibrary/Program.cs
@@ -1,41 +1,58 @@
-﻿using System;
+﻿/*Bam Bohara 
+ * ITSE 1430
+ * Movie Library console Aplication
+ */
+using System;
 namespace MovieLibrary
 {
     class Program
     {
-        private static void Main ( string[] args )
+        static void Main ( string[] args )
         {
             //FunWithTypes();
             //FunWithvariables();
-        }
 
-        private static void FunWithvariables ()
-        {
-            // Declare hours of Type with inital value 
-            int hours = 10;
-            // Defintely assigned rule
-            int value = 0;
-            int calculatedVallue = value = 10;
-            // multiple variable variable declaration
-            int width, length;
-            int grade1 = 50, grade = 60;
-
-            //Block declarations
-            int x, y, z;
-            double rate1, rate2;
-            string name1, name2;
-            name2 = "";
-            //...
-            // when needed
-            //int x = 10;
-            //int y = 20;
-            //int z = x + y;
-            //double rate1 = x + 0.5;
-          }
-        static void FunWithTypes ()
-        {
+            // while loop (boolean (E) s;
+            while (true)
+            {
+                //Scope - lifetime of a variables: starts at declaration and continues end of current scope
+                char choice = DisplayMenu();
+                if (choice == 'Q')
+                    return;
+            };
+            string title = "";
+            string description = "";
+            string rating = "";
+            int duration;
 
         }
-       }
+        static char DisplayMenu ()
+        {
+            // do single statement while (E)
+            // block statement = {signle statement + }
+            do
+            {
+                Console.WriteLine("Movie Library");
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Q(uit");
 
+                //Get input from user
+                string value = Console.ReadLine();
+                // if (E) s;
+                if (value =="Q")
+                    return 'Q';
+                DisplayError();
+            } while (true);
+
+        }
+
+        private static void DisplayError ()
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Invalid option");
+
+            Console.ResetColor();
+        }
+    }
 }
