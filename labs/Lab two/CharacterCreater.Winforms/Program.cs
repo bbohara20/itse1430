c605517210ec8d241f230a1161811b2ff22ca5f3
bam bohara <bam.bohara@my.tccd.edu>
2020-10-16 12:14:33 -0500

    Story 1

--- labs/Lab two/CharacterCreater.Winforms/Program.cs
+++ labs/Lab two/CharacterCreater.Winforms/Program.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CharacterCreator.Winforms
+namespace CharacterCreater.Winforms
 {
     static class Program
     {
