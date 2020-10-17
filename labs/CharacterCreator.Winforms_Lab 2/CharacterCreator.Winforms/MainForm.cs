9068bced8dd5f8320f33db3b4d320c2f80ebb4b4
bam bohara <bam.bohara@my.tccd.edu>
2020-10-16 23:23:04 -0500

    Story 1

---
+++ labs/CharacterCreator.Winforms_Lab 2/CharacterCreator.Winforms/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CharacterCreator.Winforms
+{
+    public partial class MainForm : Form
+    {
+     public MainForm ()
+        {
+           InitializeComponent();
+
+          }
+        private void Exit ( object sender, EventArgs e )
+        {
+            Application.Exit();
+
+        }  private void menuStrip1_ItemClicked_1 ( object sender, ToolStripItemClickedEventArgs e )
+        {
+
+        }
+    }
+}
